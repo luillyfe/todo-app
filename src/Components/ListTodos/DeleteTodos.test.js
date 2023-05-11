f085ccad46c496e368790bd0f9bd7652af9e540d
Fermin Blanco <luillyfe89@gmail.com>
2023-05-11 05:21:53 -0500

    chore: enhacing delete elment test cases

--- src/Components/ListTodos/DeleteTodos.test.js
+++ src/Components/ListTodos/DeleteTodos.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 
 import rootReducer from "../../redux/reducers";
 import { createTodos } from "../../utils";
@@ -18,6 +18,21 @@ describe("DeleteTodos:", () => {
 
     // assert
     screen.getByText("X");
+    expect(screen.getByText("X")).toBeInTheDocument();
+  });
+
+  it("When click in delete, it should delete the todo", () => {
+    // arrange
+    renderWithProps(<ListTodos />, { preloadedState: { todos } });
+
+    // act
+    const button = screen.getByText("X");
+    act(() => {
+      button.click();
+    });
+
+    // assert
+    expect(screen.getByRole("list")).toBeEmptyDOMElement();
   });
 });
 
